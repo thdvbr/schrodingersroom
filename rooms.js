a1f7e5e161cbeb5e3b58d2e2203603b5dad7e8f1
puk <user@users-MacBook-Pro.local>
2018-12-09 03:35:12 +0100

    vv

---
+++ rooms.js
@@ -0,0 +1,50 @@
+var rooms = [
+    {
+        "name" : 'spock',
+        "status" : 'booked'
+    },
+    {
+        "name" : 'paper',
+        "status" : 'booked'
+    },
+    {
+        "name" : 'rock',
+        "status" : 'not-booked'
+    },
+    {
+        "name" : "scissors",
+        "status" : "not-booked"
+    },
+    {
+        "name" : "lizard",
+        "status" : "not-booked"
+    },
+    {
+        "name" : "roomy-mcroomface",
+        "status" : "booked"
+    },
+    {
+        "name" : "rick",
+        "status" : "not-booked"
+    },
+    {
+        "name" : "morty",
+        "status" : "not-booked"
+    },
+    {
+        "name" : "amy-farrah-fowler",
+        "status" : "not-booked"
+    },
+    {
+        "name" : "nymeria",
+        "status" : "booked"
+    },
+    {
+        "name" : "cognito",
+        "status" : "not-booked"
+    },
+    {
+        "name" : "6-minutes",
+        "status" : "booked"
+    }
+]
