aba787cb5d29aa7c19813d8c3de5a105ebdac7d1
puk <user@users-MacBook-Pro.local>
2018-12-10 00:50:00 +0100

    v

--- rooms.js
+++ rooms.js
@@ -1,50 +1,62 @@
 var rooms = [
     {
         "name" : 'spock',
-        "status" : 'booked'
+        "status" : 'booked',
+        "occupation" : 'true'
     },
     {
         "name" : 'paper',
-        "status" : 'booked'
+        "status" : 'booked',
+        "occupation" : 'false'
     },
     {
         "name" : 'rock',
-        "status" : 'not-booked'
+        "status" : 'not-booked',
+        "occupation" : 'true'
     },
     {
         "name" : "scissors",
-        "status" : "not-booked"
+        "status" : "not-booked",
+        "occupation" : 'false'
     },
     {
         "name" : "lizard",
-        "status" : "not-booked"
+        "status" : "not-booked",
+        "occupation" : 'false'
     },
     {
         "name" : "roomy-mcroomface",
-        "status" : "booked"
+        "status" : "booked",
+        "occupation" : 'true'
     },
     {
         "name" : "rick",
-        "status" : "not-booked"
+        "status" : "not-booked",
+        "occupation" : 'false'
     },
     {
         "name" : "morty",
-        "status" : "not-booked"
+        "status" : "booked",
+        "occupation" : 'false'
     },
     {
         "name" : "amy-farrah-fowler",
-        "status" : "not-booked"
+        "status" : "not-booked",
+        "occupation" : 'true'
     },
     {
         "name" : "nymeria",
-        "status" : "booked"
+        "status" : "booked",
+        "occupation" : 'true'
     },
     {
         "name" : "cognito",
-        "status" : "not-booked"
+        "status" : "not-booked",
+        "occupation" : 'false'
     },
     {
         "name" : "6-minutes",
-        "status" : "booked"
+        "status" : "booked",
+        "occupation" : 'false'
     }
 ]
